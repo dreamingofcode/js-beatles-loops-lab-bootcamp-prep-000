fa318dd144bded0d1c41c4dc19d460f55839b404
cesar mota <cesarmota@ymail.com>
2019-01-24 02:44:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ var newfactsarray=[]
 }
 
 function iLoveTheBeatles(n){
-  var empty=[]}
+  var empty=[]
   
   do {empty.push(`I love the Beatles`) }
   while (n>15);
   n++;
   return empty;
-
+}
